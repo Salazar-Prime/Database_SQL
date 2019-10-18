a0192e44104ed2400a04258360ca71cc6109b2e9
Salazar-prime <aggarw82@purdue.edu>
2019-10-18 19:30:52 -0400

    done trigger2: p2

--- PRJ2/trg1.sql
+++ PRJ2/trg1.sql
@@ -1,13 +1,18 @@
 /* Trigger 1 - trg_maxbooks*/
 /* Working - yet to integrate with functions */
+/* check return_date=null*/ 
 rem EE 562 Project 2
 rem Varun Aggarwal
 rem aggarw82
 
 /* clear history */
--- DROP TRIGGER trg_maxbooks; // using 'or replace' instead
-TRUNCATE TABLE Issue;
-TRUNCATE TABLE Logging;
+@dropall
+@createtable
+@populate
+
+set linesize 200;
+set pagesize 50;
+clear screen
 
 /* Create trigger*/
 CREATE OR REPLACE TRIGGER trg_maxbooks
@@ -18,7 +23,7 @@ DECLARE
 	position varchar2(20);
 BEGIN
 	SELECT status INTO position FROM Borrower WHERE borrower_id = :new.borrower_id;
-	SELECT count(*) INTO issued FROM Issue WHERE borrower_id = :new.borrower_id;
+	SELECT count(*) INTO issued FROM Issue WHERE (borrower_id = :new.borrower_id AND return_date IS null);
 	INSERT INTO logging VALUES (issued ,position);
 	IF position = 'student' THEN
 		IF issued >= 2 THEN
@@ -38,34 +43,38 @@ END;
 
 /* testing trigger*/
 
+
 -- tesing for student insertion
-INSERT INTO Issue
-VALUES (2, 2, '3-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 2, '3-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 2, '3-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 3, '3-march-05', '8-march-05');
+-- INSERT INTO Issue
+-- VALUES (2, 2, '3-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (3, 2, '4-march-05', null);
+-- UPDATE Issue SET return_date = '7-march-05' WHERE borrower_id=2 AND book_id=2;
+-- INSERT INTO Issue
+-- VALUES (4, 2, '5-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (6, 2, '5-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (5, 3, '6-march-05', null);
 
 -- testing for faculty insertion
-INSERT INTO Issue
-VALUES (2, 4, '4-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 4, '4-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 4, '4-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 4, '4-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 5, '4-march-05', '8-march-05');
+-- INSERT INTO Issue
+-- VALUES (2, 4, '7-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (2, 4, '8-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (2, 4, '9-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (2, 4, '10-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (2, 5, '11-march-05', null);
 
 -- random testing
-INSERT INTO Issue
-VALUES (2, 4, '4-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 4, '4-march-05', '8-march-05');
-INSERT INTO Issue
-VALUES (2, 2, '3-march-05', '8-march-05');	
+-- INSERT INTO Issue
+-- VALUES (2, 4, '12-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (2, 4, '13-march-05', null);
+-- INSERT INTO Issue
+-- VALUES (2, 2, '14-march-05', null);	
 SELECT * FROM Logging;
 SELECT * FROM Issue;
--- PRJ2/trg2.sql
+++ PRJ2/trg2.sql
@@ -1,4 +1,4 @@
-/* Trigger 1 - trg_maxbooks*/
+/* Trigger 2 - trg_charge*/
 /* Working - yet to integrate with functions */
 rem EE 562 Project 2
 rem Varun Aggarwal
@@ -13,6 +13,7 @@ rem aggarw82
 set linesize 200;
 set pagesize 50;
 clear screen
+
 /* Create trigger*/
 CREATE OR REPLACE TRIGGER trg_charge
 AFTER INSERT ON Issue
@@ -28,33 +29,36 @@ END;
 
 -- tesing for student insertion
 INSERT INTO Issue
-VALUES (2, 2, '3-march-05', '8-march-05');
+VALUES (2, 2, '3-march-05', null);
 INSERT INTO Issue
-VALUES (3, 2, '4-march-05', '8-march-05');
+VALUES (3, 2, '4-march-05', null);
+UPDATE Issue SET return_date = '7-march-05' WHERE borrower_id=2 AND book_id=2;
 INSERT INTO Issue
-VALUES (4, 2, '5-march-05', '8-march-05');
+VALUES (4, 2, '5-march-05', null);
 INSERT INTO Issue
-VALUES (5, 3, '6-march-05', '8-march-05');
+VALUES (5, 2, '5-march-05', null);
+INSERT INTO Issue
+VALUES (13, 3, '6-march-05', null);
 
 -- testing for faculty insertion
 INSERT INTO Issue
-VALUES (6, 4, '7-march-05', '8-march-05');
+VALUES (6, 4, '7-march-05', null);
 INSERT INTO Issue
-VALUES (7, 4, '8-march-05', '8-march-05');
+VALUES (7, 4, '8-march-05', null);
 INSERT INTO Issue
-VALUES (8, 4, '9-march-05', '8-march-05');
+VALUES (8, 4, '9-march-05', null);
 INSERT INTO Issue
-VALUES (9, 4, '10-march-05', '8-march-05');
+VALUES (9, 4, '10-march-05', null);
 INSERT INTO Issue
-VALUES (10, 5, '11-march-05', '8-march-05');
+VALUES (10, 5, '11-march-05', null);
 
 -- random testing
 INSERT INTO Issue
-VALUES (1, 4, '12-march-05', '8-march-05');
+VALUES (1, 4, '12-march-05', null);
 INSERT INTO Issue
-VALUES (11, 4, '13-march-05', '8-march-05');
+VALUES (11, 4, '13-march-05', null);
 INSERT INTO Issue
-VALUES (12, 2, '14-march-05', '8-march-05');	
+VALUES (12, 2, '14-march-05', null);	
 
 /* print debugging tables*/ 
 SELECT * FROM Logging;

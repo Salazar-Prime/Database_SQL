fa3268bc7426bfea246785be47d8293a71941282
Salazar-prime <aggarw82@purdue.edu>
2019-10-22 13:42:14 -0400

    add pro5: p2

---
+++ PRJ2/pro5.sql
@@ -0,0 +1,49 @@
+/* Procedure 5 - pro_list_popular */
+/*  */ 
+rem EE 562 Project 2
+rem Varun Aggarwal
+rem aggarw82
+
+/* clear history */
+@triggers -- triggers clears all tables et. al.
+TRUNCATE TABLE Logging;
+set linesize 200;
+set pagesize 50;
+set serveroutput ON;
+clear screen
+
+--
+--
+--
+--
+--
+
+CREATE OR REPLACE PROCEDURE pro_list_popular
+AS
+/* declare varibles if needed */
+BEGIN
+	
+	DBMS_OUTPUT.PUT_LINE(rpad('Month',15)|| rpad('Year',15)
+						 || rpad('Author Name',30)|| rpad('# of Editions',15));	
+
+	DBMS_OUTPUT.PUT_LINE(rpad('-----',15)|| rpad('----',15)
+						 || rpad('-----------',30)|| rpad('-------------',15));		
+	/* fetching data */
+	--
+	--
+	
+	/* printing output */	
+	--
+	--
+END;
+/
+
+/* testing trigger*/
+EXEC pro_list_popular;
+
+
+/* print debugging tables*/ 
+SELECT * FROM Logging;
+SELECT * FROM Issue;
+SHOW error;
+

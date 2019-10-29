82af72a5ccba58a70dda5739924ed4752c0b88c1
Salazar-prime <aggarw82@purdue.edu>
2019-10-29 16:47:13 -0400

    final triggers: p2

--- PRJ2/dropall.sql
+++ PRJ2/dropall.sql
@@ -2,12 +2,23 @@ rem EE 562 Project 2
 rem Varun Aggarwal
 rem aggarw82
 /* Drop Functions*/
+DROP FUNCTION fun_issue_book;
+DROP FUNCTION fun_issue_anyedition;
+-- fun3
 DROP FUNCTION fun_return_book;
+
+/* Drop Procedures */
+DROP PROCEDURE pro_print_borrower;
+DROP PROCEDURE pro_print_fine;
+DROP PROCEDURE pro_listborr_mon;
+DROP PROCEDURE pro_listborr;
+-- pro5
 
 /* Drop Triggers */ 
 DROP TRIGGER trg_maxbooks;
 DROP TRIGGER trg_charge;
 DROP TRIGGER trg_notcharge;
+-- trg4
 
 /* Drop all tables*/
 DROP TABLE Issue;
